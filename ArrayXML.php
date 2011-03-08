ae72ae9f73067c3223007244d67a3efdf2d971f4
Christoph Herbst <chris.p.herbst@googlemail.com>
2011-03-08 18:19:09 +0100

    ArrayXML: added function XMLToArray

--- ArrayXML.php
+++ ArrayXML.php
@@ -34,7 +34,7 @@ class ArrayXML
 			}
 			
 			// replace anything not alpha numeric
-			$key = preg_replace('/[^a-z]/i', '', $key);
+			$key = preg_replace('/[^a-z_]/i', '', $key);
 			
 			// if there is another array found recrusively call this function
 			if (is_array($value))
@@ -54,5 +54,61 @@ class ArrayXML
 		// pass back as string. or simple xml object if you want!
 		return $xml->asXML();
 	}
+
+
+	// Sherwin R. Terunez
+	//
+	// convert xml object to array
+	//
+	public static function XMLToArray($obj, $level=0) {
+   
+	    $items = array();
+   
+	    if(!is_object($obj)) return $items;
+       
+	    $child = (array)$obj;
+
+	    if(sizeof($child)>1) {
+	        foreach($child as $aa=>$bb) {
+      	         if(is_array($bb)) {
+                   foreach($bb as $ee=>$ff) {
+                     if(!is_object($ff)) {
+                         $items[$aa][$ee] = $ff;
+                     } else
+                     if(get_class($ff)=='SimpleXMLElement') {
+                        $items[$aa][$ee] = self::XMLToArray($ff,$level+1);
+                     }
+                   }
+                } else
+                if(!is_object($bb)) {
+                  $items[$aa] = $bb;
+                } else
+                if(get_class($bb)=='SimpleXMLElement') {
+                  $items[$aa] = self::XMLToArray($bb,$level+1);
+                }
+            }
+          } else
+          if(sizeof($child)>0) {
+            foreach($child as $aa=>$bb) {
+              if(!is_array($bb)&&!is_object($bb)) {
+                 $items[$aa] = $bb;
+              } else
+              if(is_object($bb)) {
+                 $items[$aa] = self::XMLToArray($bb,$level+1);
+              } else {
+                 foreach($bb as $cc=>$dd) {
+                    if(!is_object($dd)) {
+                        $items[$obj->getName()][$cc] = $dd;
+                    } else
+                    if(get_class($dd)=='SimpleXMLElement') {
+                        $items[$obj->getName()][$cc] = self::XMLToArray($dd,$level+1);
+                    }
+                 }
+             }
+           }
+         }
+
+       return $items;
+    } 
 }
 ?>
